a89533fe28cddf5f0d339149af453e6cc499bf6c
ChekinVR <vidarte.devstudio@gmail.com>
2023-09-05 20:52:33 -0700

    Logic an Programm Complete

---
+++ src/Kaprekar.java
@@ -0,0 +1,59 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+public class Kaprekar {
+
+    public static String validationNumber(String n, int maxLength) {
+        Set<Character> number = new HashSet<>();
+
+        for (char c : n.toCharArray()) {
+            if (!number.add(c)) {
+                throw new IllegalArgumentException("Número no válido: Números repetidos de manera consecutiva");
+            }
+        }
+        if (n.length() > maxLength) {
+            throw new IllegalArgumentException("Número no válido: Longitud incorrecta");
+        }
+        return n;
+    }
+    public static void KaprekarConstant(int num) {
+        if (num < 1000 || num >= 10000) {
+            throw new IllegalArgumentException("El número debe ser un entero de 4 dígitos.");
+        }
+
+        int numVeces = 0;
+        while (num != 6174) {
+            int[] numArr = new int[4];
+
+            // Convertir el número en un arreglo de dígitos
+            for (int i = 0; i < 4; i++) {
+                numArr[i] = num % 10;
+                num /= 10;
+            }
+
+            // Ordenar el arreglo de mayor a menor y de menor a mayor
+            Arrays.sort(numArr);
+            int menor = numArr[0] * 1000 + numArr[1] * 100 + numArr[2] * 10 + numArr[3];
+            int mayor = numArr[3] * 1000 + numArr[2] * 100 + numArr[1] * 10 + numArr[0];
+
+            num = mayor - menor;
+            numVeces++;
+            System.out.println("Resta: " + mayor + " - " + menor + " = " + num);
+        }
+
+        System.out.println("El resultado es: " + num);
+        System.out.println("Se restó " + numVeces + " veces.");
+    }
+
+    public static String putZeros(String n) {
+        if (n.length() > 4) {
+            throw new IllegalArgumentException("Número no válido: Es mayor de 4 dígitos");
+        } else {
+            String formattedNumber = String.format("%d", Integer.parseInt(n));
+            int numberOfZerosToAdd = 4 - formattedNumber.length();
+            return formattedNumber + "0".repeat(Math.max(0, numberOfZerosToAdd));
+        }
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 //Kaprekar Programm
 public class Main {
-    public static void main(String[] args) {
+    static Scanner sc = new Scanner(System.in);
 
+    public static void main(String[] args) {
+        String num = new String();
+        System.out.println("Put your number: ");
+        num = sc.nextLine();
+        num = Kaprekar.validationNumber(num, 4);
+        int ResultKaprekar = Integer.parseInt(Kaprekar.putZeros(num));
+        Kaprekar.KaprekarConstant(ResultKaprekar);
     }
 }
