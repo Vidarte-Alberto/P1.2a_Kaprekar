c9ea6d634f701f1bd75381718319132f651972d2
ChekinVR <vidarte.devstudio@gmail.com>
2023-09-05 20:53:28 -0700

    Remove redundances

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        String num = new String();
+        String num;
         System.out.println("Put your number: ");
         num = sc.nextLine();
         num = Kaprekar.validationNumber(num, 4);
